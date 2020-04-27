328e36884216cdbbfd9e5c3539d84b7b64be67d6
amaslovskiy <F2329ea01k4>
2020-04-27 17:37:21 +0300

    DEV - словари

---
+++ src/test/java/ramzet89/dictionary/TinkoffTest.java
@@ -0,0 +1,11 @@
+package ramzet89.dictionary;
+
+public class TinkoffTest {
+    int startIndex = 0;
+
+      for (int i=0; i< A.length; i++) {
+        if (Math.abs(A[i])<Math.abs(A[startIndex])) {
+            startIndex = i;
+        }
+    }
+}
